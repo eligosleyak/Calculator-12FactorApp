a6124eee1c36b5d1d103b755cd2f4eab3b2f3413
Shashank Acharya <shashankchr@protonmai.ch>
2025-05-14 18:25:39 +0545

    Added other basic features for calculator

--- app/main.py
+++ app/main.py
@@ -11,3 +11,18 @@ async def welcome():
 @app.get("/add/{a}/{b}")
 async def add(a: int, b: int):
     return {"result": a + b}
+
+
+@app.get("/subtract/{a}/{b}")
+async def subtract(a: int, b: int):
+    return {"result": a - b}
+
+
+@app.get("/multiply/{a}/{b}")
+async def multiply(a: int, b: int):
+    return {"result": a * b}
+
+
+@app.get("/divide/{a}/{b}")
+async def divide(a: int, b: int):
+    return {"result": a / b}
